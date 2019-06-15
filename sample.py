791d2911865283fb14466862bec084dbd2f54d11
unknown <mallappasiragur696628>
2019-06-15 11:52:36 +0530

    addedd duplication of printf ststements

--- sample.py
+++ sample.py
@@ -13,6 +13,13 @@ print("d", d)
 print("addition of two numbers")
 print("substraction of two numbers")
 
+print("addition1 of two numbers")
+print("substractio1n of two numbers")
+print("addi1tion of two numbers")
+print("substract1ion of two numbers")
+print("addi1tio1n of two numbers")
+print("subs1traction of two numbers")
+
 
 
 
