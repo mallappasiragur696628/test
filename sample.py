313a169993ea15d32aad2e2271611458ce9147d7
unknown <mallappasiragur696628>
2019-06-15 12:19:40 +0530

    sample copy branch

--- sample.py
+++ sample.py
@@ -13,12 +13,7 @@ print("d", d)
 print("addition of two numbers")
 print("substraction of two numbers")
 
-print("addition1 of two numbers")
-print("substractio1n of two numbers")
-print("addi1tion of two numbers")
-print("substract1ion of two numbers")
-print("addi1tio1n of two numbers")
-print("subs1traction of two numbers")
+print("which is present in sample copy branch")
 
 
 
